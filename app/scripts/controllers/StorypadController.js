5b83d949112171d36c2e988fdf75e7725eb9c246
Veerakumar <veerakumarcse24@gmail.com>
2019-02-26 10:02:55 +0530

    for textarea'

--- app/scripts/controllers/StorypadController.js
+++ app/scripts/controllers/StorypadController.js
@@ -12,34 +12,47 @@
         var self = this;
         self.scope = $scope;
         self.rootScope = $rootScope;
+
+        vmStorypad.onInit = function()
+        {
+            vmStorypad.tab = 1;
+        }
+
+        vmStorypad.storyTabChange = function(tab)
+        {
+            vmStorypad.tab = tab;
+        }
+
+        vmStorypad.onFullScreenCallback = function(e) {
+            e.showPreview();
+        }
+
+        vmStorypad.fullScreenPreview = function() {
+            $rootScope.markdownEditorObjects.editor1.showPreview();
+            $rootScope.markdownEditorObjects.editor1.setFullscreen(true);
+        }
+
+        vmStorypad.saveStoryFile = function() {
+          // var json = vmIntentGrid.trainData;
+          // var jsonse = JSON.stringify(json);
+          var blob = new Blob([vmStorypad.storiesFile], {
+            type: "application/text"
+          });
+          $scope.filename = $scope.filename || "my_stories";
+          saveAs(blob, $scope.filename + ".md");
+        }
+
+        vmStorypad.saveConfigFile = function() {
+          // var json = vmIntentGrid.trainData;
+          // var jsonse = JSON.stringify(json);
+          var blob = new Blob([vmStorypad.configFile], {
+            type: "application/text"
+          });
+          $scope.filename = $scope.filename || "my_stories";
+          saveAs(blob, $scope.filename + ".md");
+        }
        
-        //vmIntentGrid.onInit();
+        vmStorypad.onInit();
     }
 
 })();
-
-
-angular.module('optisolNluTrainerApp').directive('ckEditor', function () {
-    return {
-        require: '?ngModel',
-        link: function (scope, elm, attr, ngModel) {
-            var ck = CKEDITOR.replace(elm[0]);
-            if (!ngModel) return;
-            ck.on('instanceReady', function () {
-                ck.setData(ngModel.$viewValue);
-            });
-            function updateModel() {
-                scope.$apply(function () {
-                ngModel.$setViewValue(ck.getData());
-            });
-        }
-        ck.on('change', updateModel);
-        ck.on('key', updateModel);
-        ck.on('dataReady', updateModel);
-
-        ngModel.$render = function (value) {
-            ck.setData(ngModel.$viewValue);
-        };
-    }
-};
-});
